b331ff7fdd9ce34380bd5b3f019f697d4700e056
ianpgo <ianpgo@gmail.com>
2014-12-08 22:43:56 -0500

    added player card page

--- routes/serverSocket.js
+++ routes/serverSocket.js
@@ -1,29 +1,23 @@
-var Host = require('../models/Host.js');
-var Player = require('../models/Player.js');
-var deck = require("../models/mymongo.js");
+// var Host = require('../models/Host.js');
+// var Player = require('../models/Player.js');
+// var deck = require("../models/mymongo.js");
 
-var fs = require('fs');
+// var fs = require('fs');
 
-var playerNames = [];
-var playerHost = false; //if true player is Host 
+// var playerNames = [];
+// var playerHost = false; //if true player is Host 
 
-exports.init = function(io){
-	var questionDeck = [];
-	var answerDeck = []; 
-	io.sockets.on('connection', function (socket) {
+// exports.init = function(io){
+// 	var questionDeck = [];
+// 	var answerDeck = []; 
+// 	io.sockets.on('connection', function (socket) {
 
 
-		socket.on("startGame", function(data){
+// 		socket.on("startGame", function(data){
 
-			function readLines(input, func)
+// 			function readLines(input, func)
 
-			//POPULATE DECK
-			// deck.findAll("whitecards","",
-			// 		function(whitecards){
+// 		})
 
-			// 		}
-			// 	)
-		})
-
-	});
-}
+// 	});
+// }

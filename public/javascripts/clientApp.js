8031984941b320d734635d460346e03734eb4a79
ianpgo <ianpgo@gmail.com>
2014-12-10 22:34:13 -0500

    added move effect on cards

--- public/javascripts/clientApp.js
+++ public/javascripts/clientApp.js
@@ -2,12 +2,14 @@
 
 		$("#username").focus();
 
+		//Make white button black on hover
 		$(".whiteButton").on("mouseenter", function(){
 			$(this).css("background-color","black").css("color","white").css("border","1px solid white");
 		}).on("mouseleave",function(){
 			$(this).css("background-color","white").css("color","black").css("border","0px");
 		});
 
+		//Put in answer field on click
 		$(".answerRow").on("click", ".cards", function(){
 			var tempAnswer= $(this).text();
 			$(".answer-field").hide().text(tempAnswer).fadeIn();
@@ -15,10 +17,18 @@
 			clearInterval(blinkerAnswer);
 		});
 
+		//submit button turns black on click
 		$(".answerRow").on("mouseenter", ".cards-submit", function(){
 			$(this).css("background-color","black").css("color","white").css("border","1px solid white");
 		}).on("mouseleave", ".cards-submit", function(){
 			$(this).css("background-color","white").css("color","black").css("border","0px");
+		});
+
+		//card move effect on hover
+		$(".answerRow").on("mouseenter", ".cards", function(){
+			$(this).css("position","relative").css("bottom","25px");
+		}).on("mouseleave", ".cards", function(){
+			$(this).css("position","static").css("bottom","0");
 		});
 
 		var blinkerAnswer = setInterval(function(){
